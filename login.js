525d852eab4b564b195f97fc73e4811dbdf832d8
Anas Pawaskar <pawaskaranas10@gmail.com>
2024-03-09 12:05:33 +0530

    creating a javascript project

---
+++ login.js
@@ -0,0 +1,28 @@
+
+console.log("anas pawaskar learning java script");
+name = "anas pawaskar"
+console.log(name);
+age = 24
+console.log(age,name);
+
+const student = {
+    "Name":"anas",
+    "age": 20,
+    "marks": 7.88,
+    "ispass": true
+}
+
+console.log(student);
+
+
+const profile = {
+    "username": "anas pawaskar",
+    "no_post" : 0,
+    "isfollow" : true,
+    "following" : 1234,
+    "follower" : 45000
+}
+
+console.log(profile);
+
+console.log(typeof profile["username"]);
